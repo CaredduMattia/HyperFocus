7e097c622fc58b73c4f451339063b6d6599bf201
Domenico Amato <amatonico2001@gmail.com>
2023-04-23 12:47:35 +0200

    aggiustata sezione email con css e js, modificate alcune icone con immagini senza sfondo

--- JS/main.js
+++ JS/main.js
@@ -74,8 +74,8 @@ window.addEventListener("scroll", () => {
 
 // cursore negativo
 
+let cursor = document.getElementById('myCustomCursor');
 function createCustomCursor() {
-    let cursor = document.getElementById('myCustomCursor');
 
     if (cursor) {
         console.log('myCustomCursor already exist');
@@ -121,4 +121,63 @@ function addCursorSpecialEffectToAllPageLinks(cursor) {
 createCustomCursor();
 
 // fine cursore negativo
+
+
+// Section email 
+
+let inputEmail = document.querySelector(`#inputEmail`);
+
+let singUpCustom = document.querySelector(`#singUpCustom`);
+
+let buttonEmailCustom = document.querySelector(`.button-email-custom`);
+
+// inizio evento input email
+
+inputEmail.addEventListener(`mouseenter`, ()=>{
+
+    inputEmail.style.background = ` rgba(128, 128, 128, 0.093)`;
+  
+    inputEmail.style.cursor = `none`;
+
+    inputEmail.style.transition = `0.4s`;
+
+        
+})
+
+inputEmail.addEventListener(`mouseleave`, ()=>{
+
+  
+    inputEmail.style.background = `white`;
+})
+
+// fine evento input email 
+
+  
+// Inizio evento bottone sign up
+   
+buttonEmailCustom.addEventListener(`mouseenter`, ()=>{
+
+    singUpCustom.style.color = `grey`;
+
+    singUpCustom.style.cursor = `none`;
+
+    singUpCustom.style.transition = `0.4s`;
+
+    cursor.style.transform = `scale(1.3)`;
+
+})
+
+buttonEmailCustom.addEventListener(`mouseleave`, ()=>{
+
+    singUpCustom.style.color= `black`;
+
+    singUpCustom.style.transition = `0.4s`;
+
+    cursor.style.transform = `scale(1)`;
+    
+})
+
+// fine evento bottone sign up
+  
+
 
