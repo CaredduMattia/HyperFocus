5c7e0cf37372fce296458fa9a3b16276d1c1a1e1
PaoloPetrassi <128042245+PaoloPetrassi@users.noreply.github.com>
2023-04-24 15:18:33 +0200

    modifiche applicate

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,54 @@
 
+// cursore negativo
+
+let cursor = document.getElementById('myCustomCursor');
+function createCustomCursor() {
+
+    if (cursor) {
+        console.log('myCustomCursor already exist');
+        //addCursorSpecialEffectToAllPageLinks(cursor);
+    } else {
+        cursor = document.createElement("div");
+        cursor.setAttribute("id", "myCustomCursor");
+        document.body.appendChild(cursor);
+
+        initCustomCursor(cursor);
+        //addCursorSpecialEffectToAllPageLinks(cursor);
+    }
+}
+
+
+function initCustomCursor(cursor) {
+    document.body.onmousemove = function (e) {
+        cursor.style.setProperty('--x', (e.clientX) + 'px');
+        cursor.style.setProperty('--y', (e.clientY) + 'px');
+    }
+}
+
+function addCursorSpecialEffectToAllPageLinks(cursor) {
+    var links = document.querySelectorAll("a");
+
+    // This for loop is used to find all the page links and add the "myCursorHoverState" css class to create special effect on hover
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i].getAttribute("href");
+        console.log(link);
+
+        links[i].addEventListener("mouseenter", function (event) {
+            console.log('In');
+            cursor.classList.add("myCursorHoverState");
+        }, false);
+
+        links[i].addEventListener("mouseleave", function (event) {
+            console.log('Out');
+            cursor.classList.remove("myCursorHoverState");
+        }, false);
+    }
+}
+
+createCustomCursor();
+
+// fine cursore negativo
+
 // autoplay video 1
 let check = true;
 let check2 = true;
@@ -68,55 +118,6 @@ window.addEventListener("scroll", () => {
     scrollvalue = window.scrollY;
 })
 
-// cursore negativo
-
-let cursor = document.getElementById('myCustomCursor');
-function createCustomCursor() {
-
-    if (cursor) {
-        console.log('myCustomCursor already exist');
-        //addCursorSpecialEffectToAllPageLinks(cursor);
-    } else {
-        cursor = document.createElement("div");
-        cursor.setAttribute("id", "myCustomCursor");
-        document.body.appendChild(cursor);
-
-        initCustomCursor(cursor);
-        //addCursorSpecialEffectToAllPageLinks(cursor);
-    }
-}
-
-
-function initCustomCursor(cursor) {
-    document.body.onmousemove = function (e) {
-        cursor.style.setProperty('--x', (e.clientX) + 'px');
-        cursor.style.setProperty('--y', (e.clientY) + 'px');
-    }
-}
-
-function addCursorSpecialEffectToAllPageLinks(cursor) {
-    var links = document.querySelectorAll("a");
-
-    // This for loop is used to find all the page links and add the "myCursorHoverState" css class to create special effect on hover
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i].getAttribute("href");
-        console.log(link);
-
-        links[i].addEventListener("mouseenter", function (event) {
-            console.log('In');
-            cursor.classList.add("myCursorHoverState");
-        }, false);
-
-        links[i].addEventListener("mouseleave", function (event) {
-            console.log('Out');
-            cursor.classList.remove("myCursorHoverState");
-        }, false);
-    }
-}
-
-createCustomCursor();
-
-// fine cursore negativo
 
 
 // Section email 
